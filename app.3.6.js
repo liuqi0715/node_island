c45140f76fbacf909809f321800e89c34ae1844f
liuqi <qi.liu@cheifclouds.com>
2021-01-22 18:05:19 +0800

    add:新增websocket功能

--- app.3.6.js
+++ app.3.6.js
@@ -9,7 +9,10 @@ const router = new Router();
 
 // const modules =  requireDirective(module,'模块下的路径');
 //这里的 modules 就是所有的路由
-router.get('/classic/latest',(ctx,next)=>{
+router.get('/classic/latest/:id',(ctx,next)=>{
+    console.log(ctx,'params')//路径中的参数
+    console.log(ctx.request.body,'query')//通过raw传递的的参数
+
     ctx.body = {key:'月亮与六便士'}
     return '123'
 })
